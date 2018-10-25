671b7fff6d2e49e51e27b60677fa4f6b083eb3d6
zxxml <zxxml@outlook.com>
2018-10-25 17:05:42 +0800

    完善异常处理

--- www-data/www/patches/install_patches.py
+++ www-data/www/patches/install_patches.py
@@ -15,25 +15,34 @@ class PatchesInstaller:
 
     def install(self):
         for patch in self.files:
-            print(patch['description'])
-            if 'backup' in patch and not os.path.exists(patch['backup']):
-                if not os.path.exists(os.path.dirname(patch['backup'])):
-                    os.makedirs(os.path.dirname(patch['backup']))
-                shutil.copyfile(patch['to'], patch['backup'])
-            shutil.copyfile(patch['from'], patch['to'])
-            if 'append' in patch:
-                args = [self.args[x] for x in patch['append']['args']
-                        ] if 'args' in patch['append'] else []
-                content = patch['append']['content'].format(*args)
-                with open(patch['to'], 'a') as fp:
-                    fp.write(content)
+            try:
+                print(patch['description'])
+                # 仅当 backup 选项被启用，并且不存在已有备份时，备份文件
+                if 'backup' in patch and not os.path.exists(patch['backup']):
+                    if not os.path.exists(os.path.dirname(patch['backup'])):
+                        os.makedirs(os.path.dirname(patch['backup']))
+                    shutil.copyfile(patch['to'], patch['backup'])
+                shutil.copyfile(patch['from'], patch['to'])
+                if 'append' in patch:
+                    args = [self.args[x] for x in patch['append']['args']
+                            ] if 'args' in patch['append'] else []
+                    content = patch['append']['content'].format(*args)
+                    with open(patch['to'], 'a') as fp:
+                        fp.write(content)
+            except Exception as e:
+                print('failed to install: {}'.format(patch['description']))
+                print(e.__traceback__)
         for patch in self.dirs:
-            print(patch['description'])
-            if 'backup' in patch and not os.path.exists(patch['backup']):
-                shutil.copytree(patch['to'], patch['backup'], False)
-            if os.path.exists(patch['to']):
-                shutil.rmtree(patch['to'])
-            shutil.copytree(patch['from'], patch['to'], False)
+            try:
+                print(patch['description'])
+                if 'backup' in patch and not os.path.exists(patch['backup']):
+                    shutil.copytree(patch['to'], patch['backup'], False)
+                if os.path.exists(patch['to']):
+                    shutil.rmtree(patch['to'])
+                shutil.copytree(patch['from'], patch['to'], False)
+            except Exception as e:
+                print('failed to install: {}'.format(patch['description']))
+                print(e.__traceback__)
 
 
 if __name__ == '__main__':
